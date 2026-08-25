63f57af51c13e898182c2d6a55b3b473ab973e83
AndersonPS94 <dson_adn@hotmail.com>
2026-08-24 22:05:37 -0300

    fix: zerando o projeto para fazermos tudo do zero

--- app/src/main/java/com/jamiltonmentoria/nexusstore/MainActivity.kt
+++ app/src/main/java/com/jamiltonmentoria/nexusstore/MainActivity.kt
@@ -2,11 +2,10 @@ package com.jamiltonmentoria.nexusstore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
 import com.jamiltonmentoria.nexusstore.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -16,11 +15,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupListeners()
+    }
 
-        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+    private fun setupListeners() {
+
     }
 }
